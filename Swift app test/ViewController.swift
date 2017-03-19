7cf0d213ce87dc0856408f504bc9e3770b5684cf
Ryan Tomol <wickedix@ryans-mbp.fios-router.home>
2017-03-19 08:46:47 -0400

    Initial Commit

---
+++ Swift app test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift app test
+//
+//  Created by Ryan Tomol on 3/19/17.
+//  Copyright © 2017 Ryan Tomol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
